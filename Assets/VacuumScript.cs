f3f3b38236fe0740045dbbcff08f7774f8e794ed
HokiMester <erikadolf201@gmail.com>
2026-01-10 15:35:32 +0100

    Update VacuumScript.cs

--- Assets/VacuumScript.cs
+++ Assets/VacuumScript.cs
@@ -4,17 +4,38 @@ using UnityEngine;
 
 public class VacuumScript : MonoBehaviour
 {
+    private bool vacuuming;
+    private Collider2D col;
+    public bool Vacuuming { get => vacuuming; set => vacuuming = value; }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        col = GetComponent<Collider2D>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(){
 
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            vacuuming = true;
         }
+        else if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            vacuuming = false;
+        }
+
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("asd");   
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log("jó");
     }
 }
